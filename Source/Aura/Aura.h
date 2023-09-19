24f575e178567d5355ba1eac97a807f901d1608b
AmsD3v <angellosilvadev@gmail.com>
2023-09-19 17:37:52 -0300

    The Base Character Class

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Ams Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/AuraEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Ams Dev
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AuraEditorTarget : TargetRules
+{
+	public AuraEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "Aura" } );
+	}
+}
