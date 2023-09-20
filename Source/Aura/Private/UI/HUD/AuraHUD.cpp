ffc8ad5675844a9c39ef97ff50cab27bce0a8e25
AmsD3v <angellosilvadev@gmail.com>
2023-09-20 19:55:33 -0300

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Ams Dev
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClas);
+	Widget->AddToViewport();
+}
